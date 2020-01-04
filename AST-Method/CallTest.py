2d2d9ecf59d97c79be615099921ecb687b9cbc2a
Joshua Utting <49527676+utting98@users.noreply.github.com>
2020-01-04 13:58:58 +0000

    Added relevant files

---
+++ AST-Method/CallTest.py
@@ -0,0 +1,1 @@
+TestFunction(1,[2.2,3.3,4.4],'Hello',2.2)
---
+++ AST-Method/Test.py
@@ -0,0 +1,31 @@
+def TestFunction(a,b,c,randfloat):
+    x_value = a+a
+    y_value = randfloat/2
+    print(b,a)
+    print(c,randfloat)
+    return x_value,y_value
+#Test.py
+a=1
+e=-2
+b=2.2
+f=-3.7
+c='Hello'
+d="World"
+g=[2.2,3.3,4.4]
+matrix=[[2,2,2],[3,3,3],[4,4,4]]
+print('Hello2')
+print("Hello3")
+if(a==1):
+    print(c)
+    print(d)
+elif(b==2):
+    print("Test")
+else:
+    pass
+print('Extra')
+TestFunction(a,g,c,b)
+for i in range(0,len(g)):
+    g[i] = 1.1
+    g.append(9.9)
+test_value = g[1]
+g[3] = 5.1
