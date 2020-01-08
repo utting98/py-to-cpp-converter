0c34d5a17987f28e79e94f608f8193561f257037
Joshua Utting <49527676+utting98@users.noreply.github.com>
2020-01-08 16:34:32 +0000

    Updated to latest version

--- AST-Method/Test.cpp
+++ AST-Method/Test.cpp
@@ -1,58 +1,45 @@
-#include <vector>
-#include <string>
-#include <iostream>
-auto TestFunction (int a, std::vector<float> b, std::string c, float randfloat) {
-	int x_value = a + a;
-	float y_value = randfloat / 2;
-	std::cout << b << a << endl;
-	std::cout << c << randfloat << endl;
-	struct result {int dummy_return_0; float dummy_return_1;};
-	return result {x_value, y_value};
-}
-int main () {
-	int a = 1;
-	int e = -2;
-	float b = 2.2;
-	float f = -3.7;
-	std::string c = "Hello";
-	std::string d = "World";
-	std::vector<float> g = {2.2, 3.3, 4.4};
-	std::vector<std::vector<int>> matrix = {{2, 2, 2}, {3, 3, 3}, {4, 4, 4}};
-	std::cout << "Hello2" << endl;
-	std::cout << "Hello3" << endl;
-	if (a == 1) {
-		std::cout << c << endl;
-		std::cout << d << endl;
-		if (c == "Hello") {
-			std::cout << "Yay" << endl;
-		}
-		else {
-			std::cout << "Nay" << endl;
-		}
-	}
-	else if (b == 2) {
-		std::cout << "Test" << endl;
-		if (d == "World") {
-			std::cout << "Yay 2" << endl;
-		}
-		else {
-			std::cout << "Nay 2" << endl;
-		}
-	}
-	else {
-		continue;
-	}
-	std::cout << "Extra" << endl;
-	auto value = TestFunction(a, g, c, b);
-	for (int i = 0; i < g.size(); i++) {
-		g[i] = 1.1;
-		g.push_back(9.9);
-	}
-	float test_value = g[1];
-	g[3] = 5.1;
-	int incrementor = 1;
-	while (incrementor < 10) {
-		incrementor += 1;
-	}
-	return 0;
-}
+def TestFunction(a,b,c,randfloat):
+    x_value = a+a
+    y_value = randfloat/2
+    print(b,a)
+    print(c,randfloat)
+    return x_value,y_value
+#Test.py
+a=1
+e=-2
+b=2.2
+f=-3.7
+c='Hello'
+d="World"
+g=[2.2,3.3,4.4]
+matrix=[[2,2,2],[3,3,3],[4,4,4]]
+print('Hello2')
+print("Hello3")
+if(a==1):
+    print(c)
+    print(d)
+    if(c=='Hello'):
+        print('Yay')
+    else:
+        print('Nay')
+elif(b==2):
+    print("Test")
+    if(d=='World'):
+        print('Yay 2')
+    else:
+        print('Nay 2')
+else:
+    pass
+print('Extra')
+value = TestFunction(a,g,c,b)
+for i in range(0,len(g)):
+    g[i] = 1.1
+    g.append(9.9)
+test_value = g[1]
+g[3] = 5.1
+incrementor = 1
+boolean = True
+while(incrementor<10):
+    incrementor+=1
+if(boolean == True):
+    print('False')
